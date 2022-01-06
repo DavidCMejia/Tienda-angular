ff5deaccd938d0b88d7d0a7b127fcabd1bb6d592
David Mejia <davidcamilo0415@gmail.com>
2022-01-06 08:16:41 -0500

    se crea el Input para comunitar componente padre a hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,52 @@ export class AppComponent {
   title = 'tienda-angular';
   items = ['camisa', 'pantalon', 'zapatos'];
 
+
+  products: Product[] = [ //en products:Products[] se tipa diciendo que el array debe tener la estructura de product.model sino da error calculado hasta que cumpla
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
+
   addItem(){
     this.items.push("Nuevo item");
 
