2948f630c085a1d4e189fcce9aa7490fea9e0fcd
David Mejia <davidcamilo0415@gmail.com>
2022-01-31 14:45:20 -0500

    se agregar Guards a los modulos de admin que faltaban

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from './../core/guards/admin.guard'
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { CreateProductsFormComponent } from './components/create-products-form/create-products-form.component';
 import { EditProductsFormComponent } from './components/edit-products-form/edit-products-form.component'
+import { ProductsComponent } from '../products/products-components/products.component';
+
 
 const routes: Routes = [
   {
     path: '',
     component: NavComponent,
+    canActivate: [AdminGuard],
     children:[
       {
         path: 'create',
@@ -26,6 +30,10 @@ const routes: Routes = [
       {
         path:'products/edit/:id',
         component: EditProductsFormComponent
+      },
+      {
+        path:'productsPreview',
+        component: ProductsComponent
       }
 
     ]
